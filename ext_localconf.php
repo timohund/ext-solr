f802cf842df9b2dfda12bc6d792e9454d7696f2c
Timo Schmidt <timo.schmidt@dkd.de>
2015-12-02 16:11:25 +0100

    Added check if compatibility extension is installed

--- ext_localconf.php
+++ ext_localconf.php
@@ -245,23 +245,28 @@ $TYPO3_CONF_VARS['FE']['eID_include']['tx_solr_api'] = 'EXT:solr/Classes/Eid/Api
 # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- #
 
 // replace the built-in search content element
+$hasCompatibilityLayer = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('compatibility6');
+$isTYPO3Version6 = version_compare(TYPO3_branch, 7, '<');
 
-$searchReplacementTypoScript = trim('
-tt_content.search = COA
-tt_content.search {
-	10 = < lib.stdheader
-	20 >
-	20 = < plugin.tx_solr_PiResults_Results
-	30 >
+$registerSearchContentElement = $hasCompatibilityLayer || $isTYPO3Version6;
+if ($registerSearchContentElement) {
+    $searchReplacementTypoScript = trim(
+        'tt_content.search = COA' . PHP_EOL .
+        'tt_content.search {' . PHP_EOL .
+        '    10 = < lib.stdheader' . PHP_EOL .
+        '    20 >' . PHP_EOL .
+        '    20 = < plugin.tx_solr_PiResults_Results' . PHP_EOL .
+        '    30 >' . PHP_EOL .
+        '}'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        $_EXTKEY,
+        'setup',
+        '# Setting ' . $_EXTKEY . ' plugin TypoScript' . $searchReplacementTypoScript,
+        43
+    );
 }
-');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    $_EXTKEY,
-    'setup',
-    '# Setting ' . $_EXTKEY . ' plugin TypoScript' . $searchReplacementTypoScript,
-    43
-);
 
 # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- #
 
