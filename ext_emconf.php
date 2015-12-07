c9ab66d0b8688ff601c943da101244d3ce6379b6
Timo Schmidt <timo.schmidt@dkd.de>
2015-12-07 15:27:52 +0100

    Fixed deprecations of getPageRenderer and remove TYPO3 6.2.15 from travis, since this will not be supported in 3.2.

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,8 +27,8 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'scheduler' => '',
-            'php' => '5.3.7',
-            'typo3' => '6.2.1-7.99.99',
+            'php' => '5.5.0',
+            'typo3' => '7.6.0-7.99.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
