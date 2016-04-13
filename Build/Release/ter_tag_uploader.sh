42f4b4ec7e28b719406f8c2aefbe6bd2f526900d
Timo Schmidt <timo.schmidt@dkd.de>
2016-04-13 11:12:39 +0200

    Added version check before upload

--- Build/Release/ter_tag_uploader.sh
+++ Build/Release/ter_tag_uploader.sh
@@ -2,6 +2,7 @@
 #
 # See: http://insight.helhum.io/post/140850737265/automatically-upload-typo3-extensions-to-ter-with
 if [ -n "$TRAVIS_TAG" ] && [ -n "$TYPO3_ORG_USERNAME" ] && [ -n "$TYPO3_ORG_PASSWORD" ]; then
+  php Build/Release/pre_upload_check.php
   echo -e "Preparing upload of release ${TRAVIS_TAG} to TER\n"
   curl -sSL https://raw.githubusercontent.com/alrra/travis-after-all/1.4.4/lib/travis-after-all.js | node
   if [ $? -eq 0 ]; then
