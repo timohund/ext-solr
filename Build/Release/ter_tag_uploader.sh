6f51a7851e19ce62a29fc95a1cfb68eadc335747
Timo Schmidt <timo.schmidt@dkd.de>
2016-04-13 10:42:28 +0200

    [TASK] Trigger TER Release for tag after green travis build
    * Restructure of build scripts to /Build/
    * Update documentation
    * Added "namelesscoder/typo3-repository-client": "1.1.x-dev" as require-dev dependency

---
+++ Build/Release/ter_tag_uploader.sh
@@ -0,0 +1,14 @@
+# This script is triggered by travis when a build has been triggered and was tagged
+#
+# See: http://insight.helhum.io/post/140850737265/automatically-upload-typo3-extensions-to-ter-with
+if [ -n "$TRAVIS_TAG" ] && [ -n "$TYPO3_ORG_USERNAME" ] && [ -n "$TYPO3_ORG_PASSWORD" ]; then
+  echo -e "Preparing upload of release ${TRAVIS_TAG} to TER\n"
+  curl -sSL https://raw.githubusercontent.com/alrra/travis-after-all/1.4.4/lib/travis-after-all.js | node
+  if [ $? -eq 0 ]; then
+     # Cleanup before we upload
+     git reset --hard HEAD && git clean -fx
+     TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed 's/^[0-9.]*[ ]*//g'`
+     echo "Uploading release ${TRAVIS_TAG} to TER"
+     .Build/bin/upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
+  fi;
+fi;
